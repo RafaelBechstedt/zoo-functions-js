136085b25dc1db9955f3ec3189e5ea305fd1249b
Rafael <bechstedt@hotmail.com>
2022-07-10 20:57:54 -0300

    Requisito 3 - Hoje a noite será longa

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const employeeInfos = employees.find((element) => element.firstName === employeeName
+    || element.lastName === employeeName);
+  return employeeInfos;
 }
 
 module.exports = getEmployeeByName;
