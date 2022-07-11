7f511b4b02c33fec13c7af5f1d668892f4e5f9c5
Rafael <bechstedt@hotmail.com>
2022-07-11 00:27:25 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const noParam = {};
+    species.forEach((element) => {
+      noParam[element.name] = element.residents.length;
+    });
+    return noParam;
+  }
+  if (!animal.sex) {
+    return species.find((element) => element.name === animal.specie).residents.length;
+  }
+  const specie = species.find((element) => element.name === animal.specie);
+  return specie.residents.filter((element) => element.sex === animal.sex).length;
 }
 
 module.exports = countAnimals;
