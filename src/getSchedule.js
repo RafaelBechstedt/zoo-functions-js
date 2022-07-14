8358c64e4cfb401278d160e8747008d9ab4c622f
Rafael <bechstedt@hotmail.com>
2022-07-14 18:28:33 -0300

    Requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+const days = Object.keys(hours);
+const horas = Object.values(hours);
+
+const schedule = (param) => {
+  if (!param) {
+    return days.reduce((acc, current, index) => {
+      if (current === 'Monday') {
+        acc[current] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+      } else {
+        acc[current] = {
+          officeHour: `Open from ${horas[index].open}am until ${horas[index].close}pm`,
+          exhibition: species.filter((element) =>
+            element.availability.includes(current)).map((element) => element.name),
+        };
+      }
+      return acc;
+    }, {});
+  }
+};
+
+const scheduleDay = (target) => ({ [target]: schedule()[target] });
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) {
+    return schedule();
+  }
+  if (!(species.some((element) => element.name === scheduleTarget)
+    || days.some((element) => element === scheduleTarget))) {
+    return schedule();
+  }
+  if (days.includes(scheduleTarget)) {
+    return scheduleDay(scheduleTarget);
+  }
+  return species.find((element) => element.name === scheduleTarget).availability;
 }
 
 module.exports = getSchedule;
